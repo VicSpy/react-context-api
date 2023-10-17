a337878126d19dca75e64743b0857568b310e80e
Victor Kruger <98160719+VicSpy@users.noreply.github.com>
2023-10-17 08:06:20 -0300

    Inital commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,52 @@
+import { Button } from '@material-ui/core';
+import {
+  Container,
+  Titulo,
+  InputContainer
+} from './styles';
+import {
+  Input,
+  InputLabel,
+  InputAdornment
+} from '@material-ui/core';
+
+function Login({ nome, setNome }) {
+  return (
+    <Container>
+      <Titulo>
+        Insira o seu nome
+      </Titulo>
+      <InputContainer>
+        <InputLabel>
+          Nome
+        </InputLabel>
+        <Input
+          value={nome}
+          onChange={(event) => setNome(event.target.value)}
+          type="text"
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel>
+          Saldo
+        </InputLabel>
+        <Input
+          type="number"
+          startAdornment={
+            <InputAdornment position="start">
+              R$
+            </InputAdornment>
+          }
+        />
+      </InputContainer>
+      <Button
+        variant="contained"
+        color="primary"
+      >
+        Avançar
+      </Button>
+    </Container>
+  )
+};
+
+export default Login;
